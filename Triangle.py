7f49973621c9a5344e54fd65c1326b87de94c0bb
Ben-Weisbard <102400984+Ben-Weisbard@users.noreply.github.com>
2022-04-05 19:35:32 -0500

    Update Triangle.py

--- Triangle.py
+++ Triangle.py
@@ -1,15 +1,50 @@
+import random
 import turtle
 import time
+import math
 
 turtle.hideturtle()
+turtle.speed(10)
+turtle.color("blue")
 
-points = [100, 100, 50, 0, 150, 0]
+points = [[0, 0], [100, 0], [50, 74]]
+
+def dot():
+    turtle.forward(1)
 
 for x in range(0, 3):
     turtle.up()
-    turtle.goto(points[x], points[x + 1])
+    turtle.goto(points[0])
+    del points[0]
     turtle.down()
-    turtle.forward(1)
-    x + 1
+    dot()
 
-time.sleep(10)
+def newPoint():
+    ran = random.randint(1, 3)
+
+    if(ran == 1):
+        point = [0, 0]
+    if(ran == 2):
+        point = [100, 0]
+    else:
+        point = [50, 74]
+    return point
+
+def getMid(p1,p2):
+    return ((p1[0] + p2[0]) / 2, (p1[1] + p2[1]) / 2)
+
+def printAll():
+    point = newPoint()
+    print(getMid(point, turtle.pos()))
+
+def draw():
+    turtle.up()
+    turtle.goto(getMid(newPoint(), turtle.pos()))
+    turtle.down()
+    dot()
+
+for x in range(0, 2000):
+    draw()
+
+window = turtle.Screen()
+window.exitonclick()
